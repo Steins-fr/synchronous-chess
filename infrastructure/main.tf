514f8c0eefd3a7db03142837b86e2e2d2882ee21
Steins <shinato.petit@gmail.com>
2019-10-13 18:36:41 +0200

    Terraform: Use modules for lambda API.

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -1,27 +1,3 @@
-locals {
-  archive_path_ws_onconnect    = ".out/sc_ws_lambda_onconnect.zip"
-  archive_path_ws_ondisconnect = ".out/sc_ws_lambda_onconnect.zip"
-  archive_path_ws_sendmessage  = ".out/sc_ws_lambda_sendmessage.zip"
-}
-
-data "archive_file" "sc_ws_lambda_onconnect_archive" {
-  type        = "zip"
-  output_path = local.archive_path_ws_onconnect
-  source_dir  = "../application/backend/websocket-api/onconnect"
-}
-
-data "archive_file" "sc_ws_lambda_ondisconnect_archive" {
-  type        = "zip"
-  output_path = local.archive_path_ws_ondisconnect
-  source_dir  = "../application/backend/websocket-api/ondisconnect"
-}
-
-data "archive_file" "sc_ws_lambda_sendmessage_archive" {
-  type        = "zip"
-  output_path = local.archive_path_ws_sendmessage
-  source_dir  = "../application/backend/websocket-api/sendmessage"
-}
-
 data "aws_iam_policy_document" "sc_room_table" {
   statement {
     sid = "1"
@@ -64,6 +40,7 @@ EOF
   tags = {
     Name        = "iam_sc_ws_lambda_logs"
     Application = "Synchronous chess"
+    Tool        = "Terraform"
     Environment = "test"
   }
 }
@@ -89,6 +66,7 @@ EOF
   tags = {
     Name        = "iam_sc_ws_lambda_dynamo"
     Application = "Synchronous chess"
+    Tool        = "Terraform"
     Environment = "test"
   }
 }
@@ -127,57 +105,40 @@ resource "aws_dynamodb_table" "sc_database_rooms" {
   tags = {
     Name        = "sc_database_rooms"
     Application = "Synchronous chess"
+    Tool        = "Terraform"
     Environment = "test"
   }
 }
 
-resource "aws_lambda_function" "sc_ws_lambda_onconnect" {
-  filename      = local.archive_path_ws_onconnect
-  function_name = "sc_ws_lambda_onconnect"
-  role          = aws_iam_role.iam_sc_ws_lambda_logs.arn
-  handler       = "app.handler"
+module "sc_lambda_onconnect" {
+  source = "./modules/aws-lambda-api"
 
-  source_code_hash = data.archive_file.sc_ws_lambda_onconnect_archive.output_base64sha256
+  domain = "websocket-api"
+  name   = "onconnect"
+  role   = aws_iam_role.iam_sc_ws_lambda_logs.arn
+  stage  = "test"
+}
 
-  runtime = "nodejs10.x"
+module "sc_lambda_ondisconnect" {
+  source = "./modules/aws-lambda-api"
 
-  tags = {
-    Name        = "sc_ws_lambda_onconnect"
-    Application = "Synchronous chess"
-    Environment = "test"
+  domain = "websocket-api"
+  name   = "ondisconnect"
+  role   = aws_iam_role.iam_sc_ws_lambda_dynamo.arn
+  stage  = "test"
+  environment = {
+    TABLE_NAME = aws_dynamodb_table.sc_database_rooms.name
   }
 }
 
-resource "aws_lambda_function" "sc_ws_lambda_ondisconnect" {
-  filename      = local.archive_path_ws_ondisconnect
-  function_name = "sc_ws_lambda_ondisconnect"
-  role          = aws_iam_role.iam_sc_ws_lambda_dynamo.arn
-  handler       = "app.handler"
+module "sc_lambda_sendmessage" {
+  source = "./modules/aws-lambda-api"
 
-  source_code_hash = data.archive_file.sc_ws_lambda_ondisconnect_archive.output_base64sha256
-
-  runtime = "nodejs10.x"
-
-  tags = {
-    Name        = "sc_ws_lambda_ondisconnect"
-    Application = "Synchronous chess"
-    Environment = "test"
-  }
-}
-
-resource "aws_lambda_function" "sc_ws_lambda_sendmessage" {
-  filename      = local.archive_path_ws_sendmessage
-  function_name = "sc_ws_lambda_sendmessage"
-  role          = aws_iam_role.iam_sc_ws_lambda_dynamo.arn
-  handler       = "app.handler"
-
-  source_code_hash = data.archive_file.sc_ws_lambda_sendmessage_archive.output_base64sha256
-
-  runtime = "nodejs10.x"
-
-  tags = {
-    Name        = "sc_ws_lambda_sendmessage"
-    Application = "Synchronous chess"
-    Environment = "test"
+  domain = "websocket-api"
+  name   = "sendmessage"
+  role   = aws_iam_role.iam_sc_ws_lambda_dynamo.arn
+  stage  = "test"
+  environment = {
+    TABLE_NAME = aws_dynamodb_table.sc_database_rooms.name
   }
 }
