72b5c7c0bf28606f2935986ee3431fd2085f2c23
Steins <shinato.petit@gmail.com>
2019-10-31 20:27:12 +0100

    Install angular material

--- application/frontend/game-app/src/app/app.module.ts
+++ application/frontend/game-app/src/app/app.module.ts
@@ -11,6 +11,13 @@ import { RoomSetupComponent } from './components/shared/room-setup/room-setup/ro
 import { WebrtcDebugComponent } from './components/shared/debug/webrtc-debug/webrtc-debug.component';
 import { RoomApiService } from './services/room-api/room-api.service';
 import { WebSocketService } from './services/web-socket/web-socket.service';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+
+import { ScrollingModule } from '@angular/cdk/scrolling';
 
 @NgModule({
     declarations: [
@@ -23,7 +30,12 @@ import { WebSocketService } from './services/web-socket/web-socket.service';
     imports: [
         BrowserModule,
         FormsModule,
-        AppRoutingModule
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatButtonModule,
+        ScrollingModule
     ],
     providers: [RoomService, RoomApiService, WebSocketService],
     bootstrap: [AppComponent]
