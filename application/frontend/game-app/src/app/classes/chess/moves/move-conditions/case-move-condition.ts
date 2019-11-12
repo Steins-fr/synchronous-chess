b68707d230c06712f180c4afb198d0a1afd279ea
Steins <shinato.petit@gmail.com>
2019-11-12 10:41:42 +0100

    Frontend: FearHopMove replace by HopMove with DoNotApprocheMoveCondition

--- application/frontend/game-app/src/app/classes/chess/moves/move-conditions/case-move-condition.ts
+++ application/frontend/game-app/src/app/classes/chess/moves/move-conditions/case-move-condition.ts
@@ -1,12 +1,14 @@
 import Vec2 from 'vec2';
 import ChessHelper, { FenBoard } from 'src/app/helpers/chess-helper';
 import { FenPiece } from '../../piece/piece';
+import MoveCondition from './move-condition';
 
-export default class CaseMoveCondition {
+export default class CaseMoveCondition extends MoveCondition {
 
     public readonly relativePosition: Vec2;
 
     public constructor(relativePosition: Array<number>, public readonly validPieces: Array<FenPiece>) {
+        super();
         this.relativePosition = new Vec2(relativePosition);
     }
 
---
+++ application/frontend/game-app/src/app/classes/chess/moves/move-conditions/do-not-approche-move-condition.spec.ts
@@ -0,0 +1,33 @@
+import { DoNotApprocheMoveCondition } from './do-not-approche-move-condition';
+import { FenPiece } from '../../piece/piece';
+import Vec2 from 'vec2';
+import { FenBoard } from 'src/app/helpers/chess-helper';
+
+describe('DoNotApprocheMoveCondition', () => {
+    it('should create an instance', () => {
+        expect(new DoNotApprocheMoveCondition(FenPiece.BLACK_KING, 2)).toBeTruthy();
+    });
+
+    it('should say if it can move', () => {
+        // Given
+        const condition: DoNotApprocheMoveCondition = new DoNotApprocheMoveCondition(FenPiece.BLACK_KING, 2);
+        const board: FenBoard = [
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.BLACK_KING, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY],
+            [FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY, FenPiece.EMPTY]
+        ];
+
+        // When
+        const resultValidDistance: boolean = condition.canMove(new Vec2([0, 0]), new Vec2([0, 5]), board);
+        const resultInvalidDistance: boolean = condition.canMove(new Vec2([0, 0]), new Vec2([1, 1]), board);
+
+        // Then
+        expect(resultValidDistance).toBeTruthy();
+        expect(resultInvalidDistance).toBeFalsy();
+    });
+});
