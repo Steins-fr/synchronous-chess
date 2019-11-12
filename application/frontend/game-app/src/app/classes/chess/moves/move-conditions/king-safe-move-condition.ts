31b5534e979d3cf22ec9b6212ceff6dd5c177567
Steins <shinato.petit@gmail.com>
2019-11-12 15:30:18 +0100

    Frontend: King castling possible movement

--- application/frontend/game-app/src/app/classes/chess/moves/move-conditions/king-safe-move-condition.ts
+++ application/frontend/game-app/src/app/classes/chess/moves/move-conditions/king-safe-move-condition.ts
@@ -1,14 +1,28 @@
 import MoveCondition from './move-condition';
 import Vec2 from 'vec2';
-import ChessHelper, { FenBoard } from 'src/app/helpers/chess-helper';
+import ChessHelper, { FenBoard, SafeBoard } from 'src/app/helpers/chess-helper';
 import ChessRules from '../../rules/chess-rules';
 
 export class KingSafeMoveCondition extends MoveCondition {
-    public constructor(public readonly rules: ChessRules) {
+
+    public readonly andRelativeSafeCell?: Vec2;
+
+    public constructor(public readonly rules: ChessRules, andRelativeSafeCell?: Array<number>) {
         super();
+        if (andRelativeSafeCell !== undefined) {
+            this.andRelativeSafeCell = new Vec2(andRelativeSafeCell);
+        }
     }
 
-    public canMove(_oldPosition: Vec2, newPosition: Vec2, board: FenBoard): boolean {
-        return ChessHelper.fenBoardToSafeBoard(board, this.rules)[newPosition.y][newPosition.x];
+    public canMove(oldPosition: Vec2, newPosition: Vec2, board: FenBoard): boolean {
+        const safeBoard: SafeBoard = ChessHelper.fenBoardToSafeBoard(board, this.rules);
+        if (this.andRelativeSafeCell !== undefined) {
+            const andSafeCell: Vec2 = oldPosition.add(this.andRelativeSafeCell, true);
+            if (ChessHelper.isOutOfBoard(andSafeCell) === false && safeBoard[andSafeCell.y][andSafeCell.x] === false) {
+                return false;
+            }
+        }
+
+        return safeBoard[newPosition.y][newPosition.x];
     }
 }
