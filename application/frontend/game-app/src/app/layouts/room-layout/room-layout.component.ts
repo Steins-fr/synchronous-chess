afa2294680f8cc3760bcf992d871ba3d5653501b
Steins <shinato.petit@gmail.com>
2019-11-14 10:11:10 +0100

    Frontend: Added layout design
    An overlay is displayed in front of the room if the room is not active.

--- application/frontend/game-app/src/app/layouts/room-layout/room-layout.component.ts
+++ application/frontend/game-app/src/app/layouts/room-layout/room-layout.component.ts
@@ -1,10 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { RoomService } from 'src/app/services/room/room.service';
 
 @Component({
     selector: 'app-room-layout',
-    templateUrl: './room-layout.component.html'
+    templateUrl: './room-layout.component.html',
+    styleUrls: ['./room-layout.component.scss']
 })
 export class RoomLayoutComponent {
     @Input() public maxPlayer: number;
     @Input() public isDebugging: boolean = false;
+
+    public constructor(public roomService: RoomService) { }
 }
